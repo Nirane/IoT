6f76862a338003786d4b4e4d9ce71e7e49b7b8e4
Patryk <git config --global user.email patrykalkowski5675@gmail.com>
2021-11-01 19:37:42 +0100

    zmiana domeny

--- GUI/api/ApiService.cs
+++ GUI/api/ApiService.cs
@@ -12,7 +12,7 @@ namespace GUI.api
     {
         public List<SensorData> GetTempSensorData()
         {
-            var responseBody = _makeHttpGet("http://localhost:5002/sensors/temperature");
+            var responseBody = _makeHttpGet("http://api/sensors/temperature");
 
             List<SensorData> parsedList = JsonConvert.DeserializeObject<List<SensorData>>(responseBody);
 
@@ -21,7 +21,7 @@ namespace GUI.api
 
         public List<SensorData> GetHumiditySensorData()
         {
-            var responseBody = _makeHttpGet("http://localhost:5002/sensors/humidity");
+            var responseBody = _makeHttpGet("http://api/sensors/humidity");
 
             List<SensorData> parsedList = JsonConvert.DeserializeObject<List<SensorData>>(responseBody);
 
@@ -30,7 +30,7 @@ namespace GUI.api
 
         public List<SensorData> GetEthylenSensorData()
         {
-            var responseBody = _makeHttpGet("http://localhost:5002/sensors/methanol");
+            var responseBody = _makeHttpGet("http://api/sensors/methanol");
 
             List<SensorData> parsedList = JsonConvert.DeserializeObject<List<SensorData>>(responseBody);
 
@@ -40,49 +40,14 @@ namespace GUI.api
         public List<SensorData> GetPressureSensorData()
         {
             
-            var responseBody = _makeHttpGet("http://localhost:5002/sensors/pressure");
+            var responseBody = _makeHttpGet("http://api/sensors/pressure");
 
             List<SensorData> parsedList = JsonConvert.DeserializeObject<List<SensorData>>(responseBody);
 
             return parsedList;
-            // var responseBody = _makeHttpGet("https://localhost:5001/rest/json/Ethylen");
 
-            // List<Sensor> parsedList = JsonConvert.DeserializeObject<List<Sensor>>(responseBody);
-
-            // List<SensorData> mock = new List<SensorData>();
-            //
-            // mock.Add(new SensorData(id: "0", sensorId: 1, sensorType: "Temp", date: DateTime.Now,
-            //     value: Math.Round(random.NextDouble() * (100 - 1) + 1, 2)));
-            //
-            // mock.Add(new SensorData(id: "0", sensorId: 1, sensorType: "Temp", date: DateTime.Now.AddDays(1),
-            //     value: Math.Round(random.NextDouble() * (100 - 1) + 1, 2)));
-            //
-            //
-            // return mock;
         }
-
-        Random random = new Random();
-        //
-        // public List<List<Sensor>> mock()
-        // {
-        //     List<List<Sensor>> sensors = new List<List<Sensor>>();
-        //     var a = DateTime.Now;
-        //     var b = a.AddDays(1);
-        //
-        //     for (int i = 0; i < 15; i++)
-        //     {
-        //         sensors.Add(new List<Sensor>()
-        //         {
-        //             new Sensor(id: "0", sensorId: 1, sensorType: "Temp", date: b.AddDays(1),
-        //                 value: Math.Round(random.NextDouble() * (100 - 1) + 1, 2)),
-        //             new Sensor(id: "2", sensorId: 2, sensorType: "Temp", date: b.AddDays(1),
-        //                 value: Math.Round(random.NextDouble() * (100 - 1) + 1, 2)),
-        //         });
-        //     }
-        //
-        //     return sensors;
-        // }
-
+        
         public string _makeHttpGet(string url)
         {
             try
